1d804908256eb8c887bc3b56bce7fffed26445fa
minglee <leemeany@qq.com>
2019-01-13 16:22:05 +0800

    添加swagger ui

---
+++ BlogDotNetCore.Model/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BlogDotNetCore.Model
+{
+    public class Class1
+    {
+    }
+}
--- BlogDotNetCore/Startup.cs
+++ BlogDotNetCore/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace BlogDotNetCore
 {
@@ -26,6 +28,29 @@ namespace BlogDotNetCore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            #region Swagger
+
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new Info{
+                    Version = "v0.1.0",
+                    Title = "BlogDotNetCore API",
+                    Description = "接口文档",
+                    TermsOfService = "None",
+                    Contact = new Contact{
+                        Name = "liming",
+                        Email = "leemeany@qq.com",
+                        Url = ""
+                    }
+                });
+                var basePath = Microsoft.DotNet.PlatformAbstractions.ApplicationEnvironment.ApplicationBasePath;
+                //  Microsoft.Extensions.PlatformAbstractions.PlatformServices.Default.Application.ApplicationBasePath;
+                var xmlPath = Path.Combine(basePath, "BlogDotNetCore.xml");//这个就是刚刚配置的xml文件名
+                x.IncludeXmlComments(xmlPath, true);//默认的第二个参数是false，这个是controller的注释，记得修改
+            });
+
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,11 +59,26 @@ namespace BlogDotNetCore
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
+                #region Swagger
+
+                app.UseSwagger();
+                app.UseSwaggerUI(x => {
+                    x.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+                    //x.RoutePrefix = "";//路径配置，设置为空，表示直接访问该文件，
+                                       //路径配置，设置为空，表示直接在根域名（localhost:8001）访问该文件,注意localhost:8001/swagger是访问不到的，
+                                       //这个时候去launchSettings.json中把"launchUrl": "swagger/index.html"去掉， 然后直接访问localhost:8001/index.html即可
+                });
+
+
+                #endregion
             }
             else
             {
                 app.UseHsts();
             }
+
+
 
             app.UseHttpsRedirection();
             app.UseMvc();
